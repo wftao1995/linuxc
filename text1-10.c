efedb48fb3ef00b204da8ec652b98c94a82f14de
wftao1995 <wftao1995@gmail.com>
2015-10-01 15:28:48 +0000

    Bug fix

--- text1-10.c
+++ text1-10.c
@@ -9,17 +9,20 @@ struct node
     struct node *next;
 };
 typedef struct node Node;
+void sort(Node *head);
+void printlist(Node* head);
 
 int main(void)
 {
     Node *head1,*head2,*finalhead;//Head pointers of two non-decreasing lists.
     Node *creatlist(void);//Create a list.
     Node *appendlist(Node *head1,Node *head2);//append two non-decreasing lists and return a non-decreasing list.
-    void printlist(Node* head);
-    //
+        //
     head1=creatlist();
     head2=creatlist();
     finalhead=appendlist(head1,head2);
+    printlist(finalhead);
+    sort(finalhead);
     //
     printlist(finalhead);
     return 0;
@@ -58,19 +61,38 @@ Node *creatlist(void)
 	    exit(0);
 	}
     }
+    printlist(head);
     return head;
 }
 
 Node *appendlist(Node *head1, Node *head2)
 {
-    for(Node *p=head1;p->next!=NULL;p=p->next);
+    Node *p;
+    for(p=head1;p->next!=NULL;p=p->next);
     p->next=head2->next;
     return head1;
 }
 
 void printlist(Node *head)
 {
-    for(Node *p=head->next;p->next!=NULL;p=p->next)
+    for(Node *p=head->next;p!=NULL;p=p->next)
 	printf("%d ",p->num);
+    printf("\n");
 }
 
+void sort(Node *head)
+{
+    int tmp;
+    for(Node *p=head->next;p->next!=NULL;p=p->next)
+    {
+	for(Node *q=p;q->next!=NULL;q=q->next)
+	{
+	    if(q->num>q->next->num)
+	    {
+		tmp=q->num;
+		q->num=q->next->num;
+		q->next->num=tmp;
+	    }
+	}
+    }
+}
