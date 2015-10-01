b4d4a74d6df85055ea9d750f78fad848f02371c6
wftao1995 <wftao1995@gmail.com>
2015-10-01 15:40:31 +0000

    optimize

--- text1-10.c
+++ text1-10.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+typedef struct node Node;
 
 struct node
 {
     int num;
     struct node *next;
 };
-typedef struct node Node;
-void sort(Node *head);
-void printlist(Node* head);
+
+void sort(Node *head);//bubble sort
+void printlist(Node* head);//print a list
+Node *appendlist(Node *head1,Node *head2);//append two non-decreasing lists and return a non-decreasing list.
 
 int main(void)
 {
     Node *head1,*head2,*finalhead;//Head pointers of two non-decreasing lists.
     Node *creatlist(void);//Create a list.
-    Node *appendlist(Node *head1,Node *head2);//append two non-decreasing lists and return a non-decreasing list.
         //
     head1=creatlist();
     head2=creatlist();
@@ -35,41 +35,32 @@ Node *creatlist(void)
     Node *ptmp=NULL;//creat list member
     Node *pprev=head;//the pervious node
     char *toksto;//strtok_r
-    char *stok1;//Check if the list is non-decreasing
     char *stok;//Store the address returned by strtok_r
     int i;
     //
     printf("Please input a series of no-decreasing numbers separated by spaces:\n");
     fgets(buf,sizeof(buf),stdin);
-    for(i=0;buf[i]!='\n';++i);
+    for(i=0;buf[i]!='\n';++i);//exclude the '\n' readed by fgets.
     buf[i]='\0';
     stok=strtok_r(buf," ",&toksto);
-    while(1)
+    while(stok)
     {
 	ptmp=(Node *) malloc(sizeof(Node));
 	pprev->next=ptmp;//append next node to current list
 	ptmp->next=NULL;//end of new list
 	ptmp->num=atoi(stok);//if the fitst chr is not a number then there will be 0 instead.
-	pprev=ptmp;
-	stok1=stok;//check if the list is non-decreasing.
+	pprev=ptmp;//make the current one becone the pervious one in the next loop.
 	stok=strtok_r(NULL," ",&toksto);
-	if(stok==NULL)
-	    break;
-	if(strcmp(stok1,stok)>0)
-	{
-	    printf("The list is not non-decreasing!");
-	    exit(0);
-	}
     }
-    printlist(head);
+    sort(head);
     return head;
 }
 
 Node *appendlist(Node *head1, Node *head2)
 {
     Node *p;
-    for(p=head1;p->next!=NULL;p=p->next);
-    p->next=head2->next;
+    for(p=head1;p->next!=NULL;p=p->next);//locate end of the first list
+    p->next=head2->next;//append
     return head1;
 }
 
@@ -85,7 +76,7 @@ void sort(Node *head)
     int tmp;
     for(Node *p=head->next;p->next!=NULL;p=p->next)
     {
-	for(Node *q=p;q->next!=NULL;q=q->next)
+	for(Node *q=p;q->next!=NULL;q=q->next)//bubble sort
 	{
 	    if(q->num>q->next->num)
 	    {
